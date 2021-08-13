ae9b649f1733a6886cddbd146be4764b503ff8ea
StefanoBeltrame93 <stefanobeltrame12@gmail.com>
2021-08-14 00:27:44 +0200

    4th Code Commitment

--- BCRA_dashboard_app.R
+++ BCRA_dashboard_app.R
@@ -14,8 +14,6 @@ library(zoo)
 library(shinythemes)
 
 
-
-
 # Define UI for application
 ui <- fluidPage(
   theme = shinythemes::shinytheme("cosmo"),
@@ -23,7 +21,7 @@ ui <- fluidPage(
     type = "tabs",
     tabPanel(title = "Graficos",
              br(),
-             titlePanel("BCRA Dashboard for Monteary Policy"),
+             titlePanel("BCRA - Politica Monetaria"),
             br(),
             sidebarLayout(
               sidebarPanel(
@@ -31,8 +29,10 @@ ui <- fluidPage(
                             label = "Instrumento de Politica Monetaria a Visualizar",
                             choices = c("Base Monetaria" = "base",
                                         "Logaritmo Base Monetaria" = "log_bm",
+                                        "Base Monetaria en USD Corrientes" = "bm_usd",
                                         "Reservas Internacionales" = "rrii",
-                                        "Variación diaria RRII" = "delta1_rrii")),
+                                        "Variación diaria RRII" = "delta1_rrii",
+                                        "Indice MERVAL en USD" = "merval_usd")),
                 dateRangeInput(
                   inputId = "dateRange",
                   start = "1996-01-01",
@@ -44,7 +44,7 @@ ui <- fluidPage(
               ),
               
               mainPanel(
-                titlePanel(title = "Monetary Policy main Indicators"),
+                titlePanel(title = "Principales Indicadores"),
                 plotlyOutput(outputId = "graphics")
                   )
               )
@@ -69,11 +69,14 @@ server <- function(input, output) {
     )
   }
   
-  bm_resp <-  bcra_api(path = "/base")
-  rrii_resp <- bcra_api(path = "/reservas")
-  usd_resp <- bcra_api(path = "/usd")  
-  
-  
+  bm_resp <-  bcra_api(path = "/base") #Base Monetaria 
+  rrii_resp <- bcra_api(path = "/reservas") #Reservas Int
+  usd_resp <- bcra_api(path = "/usd") #Evol Tipo de Cambio
+  bm_usd_resp <- bcra_api(path = "/base_usd") #Base Monetaria % TC
+  bm_rrii_resp <- bcra_api(path = "/base_div_res") #base % RRII
+  merval_usd_resp <- bcra_api(path = "/merval_usd") #Merval % USD
+
+    
   #Base Monetaria Data
   base_monetaria_data <- 
     dplyr::as_tibble(jsonlite::fromJSON(content(bm_resp, "text", encoding = "UTF-8"))) %>% 
@@ -83,26 +86,52 @@ server <- function(input, output) {
                     delta_bm = BM - lag(BM, n = 1L),
                     daily_var_bm = delta_bm / BM)
 
-
-  
-  #Reactive RRII Data
+  #RRII Data
   rrii_data <- 
     fromJSON(content(rrii_resp, "text", encoding = NULL)) %>%
       dplyr::as_tibble() %>% 
       dplyr::rename(fecha = d, RRII = v) %>%
       dplyr::mutate(lag1_RRII = lag(x = RRII, n = 1L),
                     delta1_RRII = RRII - lag1_RRII) %>%
-      replace_na(list(lag1_RRII = 0, delta1_RRII = 0)) %>%  #Me pone un 0 en el primer valor
+      tidyr::replace_na(list(lag1_RRII = 0, delta1_RRII = 0)) %>%  #Me pone un 0 en el primer valor
       dplyr::mutate(cumsum_RRII = cumsum(delta1_RRII),
                     dailyvar_RRII = delta1_RRII / RRII,
                     fecha = lubridate::as_date(fecha))
+  
+  
+  # BM div USD 
+   bm_usd_data <-
+    jsonlite::fromJSON(content(bm_usd_resp, "text", encoding = "UTF-8")) %>% 
+    dplyr::as_tibble() %>% 
+    dplyr::rename(fecha = d, bm_usd = v) %>% 
+    dplyr::mutate(lag1_bm_usd = lag(x = bm_usd, n = 1L),
+                  delta1_bm_usd = bm_usd -lag1_bm_usd) %>% 
+    tidyr::replace_na(list(lag1_bm_usd = 0, delta1_bm_usd = 0)) %>% 
+    dplyr::mutate(cumsum_bm_usd = cumsum(delta1_bm_usd),
+                  dailyvar_bm_usd = delta1_bm_usd / bm_usd,
+                  fecha = lubridate::as_date(fecha))
+   
+  #Merval in USD 
+  merval_usd_data <- 
+    jsonlite::fromJSON(content(merval_usd_resp, "text", encoding = "UTF-8")) %>% 
+    dplyr::as_tibble() %>% 
+    dplyr::rename(fecha = d, merval_usd = v) %>% 
+    dplyr::mutate(lag1_merval_usd = lag(x = merval_usd, n = 1L),
+                  delta1_merval_usd = merval_usd - lag1_merval_usd) %>% 
+    tidyr::replace_na(list(lag1_merval_usd = 0, delta1_merval_usd = 0)) %>% 
+    dplyr::mutate(cumsum_merval_usd = cumsum(delta1_merval_usd),
+                  dailyvar_merval_usd = delta1_merval_usd / merval_usd,
+                  fecha = lubridate::as_date(fecha))
+   
   
     
   
   
   BCRA_data_base <- 
     base_monetaria_data %>% 
-    left_join(rrii_data, by = "fecha")
+    left_join(rrii_data, by = "fecha") %>% 
+    left_join(bm_usd_data, by = "fecha") %>% 
+    left_join(merval_usd_data, by = "fecha")
     
   #actualDates <- seq()
   actual_dates <- seq(from = as.Date("1996-01-01"), to = Sys.Date(), by = "day")
@@ -113,8 +142,8 @@ server <- function(input, output) {
       dplyr::rename(fecha = value) %>%
       dplyr::left_join(BCRA_data_base, by = "fecha") %>% 
       tidyr::fill(BM, .direction = c("down")) %>% 
-      tidyr::fill(c(BM, RRII, log_bm, delta1_RRII), .direction = c("up")) %>% 
-      dplyr::select(fecha, BM, RRII, log_bm, delta1_RRII)
+      tidyr::fill(c(BM, RRII, log_bm, delta1_RRII, bm_usd, delta1_bm_usd, merval_usd, delta1_merval_usd), .direction = c("up")) %>% 
+      dplyr::select(fecha, BM, RRII, log_bm, delta1_RRII, bm_usd, delta1_bm_usd, merval_usd, delta1_merval_usd)
 
     
   
@@ -132,6 +161,15 @@ server <- function(input, output) {
                                                             geom_line(mapping = aes(x = fecha, y = log_bm))+
                                                             labs(x = "fecha", y = "Logaritmo Base Monetaria")+
                                                             theme()))
+    if("bm_usd" %in% input$MPInstrument) return(ggplotly(ggplot(data = reactive_data_date_filter())+ #usamos el reactive expresion
+                                                           geom_line(mapping = aes(x = fecha, y = bm_usd))+
+                                                           labs(x = "fecha", y = "Base Monetaria en USD Corrientes")+
+                                                           theme()))
+  
+    if("merval_usd" %in% input$MPInstrument) return(ggplotly(ggplot(data = reactive_data_date_filter())+ #usamos el reactive expresion
+                                                           geom_line(mapping = aes(x = fecha, y = merval_usd))+
+                                                           labs(x = "fecha", y = "Merval en USD")+
+                                                           theme()))
     
     if("rrii" %in% input$MPInstrument) return(ggplotly(ggplot(data = reactive_data_date_filter()) + #usamos el reactive expresion
                                                 geom_line(mapping = aes(x = fecha, y = RRII)) +
